4d2c8cec9fef72c700177de4e154237cb86b29c7
jeeeeedi <jedi.reston@gritlab.ax>
2025-12-11 09:30:27 +0200

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/gritlab/paf_hackathon/PafHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.gritlab.paf_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gritlab/paf_hackathon/PafHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gritlab.paf_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
