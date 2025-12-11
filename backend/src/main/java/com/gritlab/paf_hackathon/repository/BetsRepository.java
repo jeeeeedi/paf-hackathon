734531923566b0e41b27d83628028668296d61ff
jeeeeedi <jedi.reston@gritlab.ax>
2025-12-11 14:57:47 +0200

    fix player/bets

--- backend/src/main/java/com/gritlab/paf_hackathon/repository/BetsRepository.java
+++ backend/src/main/java/com/gritlab/paf_hackathon/repository/BetsRepository.java
@@ -3,6 +3,9 @@ package com.gritlab.paf_hackathon.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.gritlab.paf_hackathon.model.Bets;
 
+import java.util.List;
+
 public interface BetsRepository extends MongoRepository<Bets, String> {
     
+    List<Bets> findByPlayerName(String playerName);
 }
